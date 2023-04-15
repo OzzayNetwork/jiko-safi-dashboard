c7891cb2e72ee93e32d83541d4ffbdec75856b97
Kelvin Njuguna <ozzaynetwork@gmail.com>
2023-04-15 23:34:29 +0300

    kelvin: the distributor statements

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -814,6 +814,8 @@ $(document).ready(function() {
 
     })
 
+    
+
     $('body').on('click','.payment-prev', function(){
         $('.payment-next').removeClass('d-none');  
         var countTheSteps = parseFloat($(".payment-panel-parent .payment-panel").length);
@@ -828,3 +830,30 @@ $(document).ready(function() {
     $('.selectpicker').selectpicker();
     $('.selectpicker').selectpicker('render')
 });
+$(document).ready(function(){
+    $(function() {
+        
+        var start = moment().subtract(29, 'days');
+        var end = moment();
+    
+        function cb(start, end) {
+            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+    
+        $('#reportrange').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+               'Today': [moment(), moment()],
+               'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+               'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+               'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+               'This Month': [moment().startOf('month'), moment().endOf('month')],
+               'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            }
+        }, cb);
+    
+        cb(start, end);
+    
+    });
+})
