1ff0bb4633054746ddc91b4125c952366dda6ea3
Kelvin Njuguna <ozzaynetwork@gmail.com>
2023-04-25 04:00:35 +0300

    kelvin: the map and gauge cg=hart

---
+++ assets/js/projectionsGauge.js
@@ -0,0 +1,91 @@
+Highcharts.chart('projections', {
+
+    chart: {
+        type: 'gauge',
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+        height: '80%'
+    },
+
+    title: {
+        text: 'Speedometer'
+    },
+
+    pane: {
+        startAngle: -90,
+        endAngle: 89.9,
+        background: null,
+        center: ['50%', '75%'],
+        size: '110%'
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0,
+        max: 200,
+        tickPixelInterval: 72,
+        tickPosition: 'inside',
+        tickColor: Highcharts.defaultOptions.chart.backgroundColor || '#FFFFFF',
+        tickLength: 20,
+        tickWidth: 2,
+        minorTickInterval: null,
+        labels: {
+            distance: 20,
+            style: {
+                fontSize: '14px'
+            }
+        },
+        plotBands: [{
+            from: 0,
+            to: 120,
+            color: '#df0012', // Red
+            thickness: 20
+        }, {
+            from: 120,
+            to: 160,
+            color: '#f7b900', // Amber
+            thickness: 20
+        }, {
+            from: 160,
+            to: 200,
+            color: '#3ea05e', // Green
+            thickness: 20
+        }]
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        tooltip: {
+            valueSuffix: ' Liters'
+        },
+        dataLabels: {
+            format: '{y} Liters',
+            borderWidth: 0,
+            color: (
+                Highcharts.defaultOptions.title &&
+                Highcharts.defaultOptions.title.style &&
+                Highcharts.defaultOptions.title.style.color
+            ) || '#333333',
+            style: {
+                fontSize: '16px'
+            }
+        },
+        dial: {
+            radius: '80%',
+            backgroundColor: 'gray',
+            baseWidth: 12,
+            baseLength: '0%',
+            rearLength: '0%'
+        },
+        pivot: {
+            backgroundColor: 'gray',
+            radius: 6
+        }
+
+    }]
+
+});
+
